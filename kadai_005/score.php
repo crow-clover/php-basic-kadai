be0a1227e0b89335578c79bdf3427b685020844d
crow-clover <150483402+crow-clover@users.noreply.github.com>
2024-02-20 11:44:27 +0900

    テストの平均点を計算しよう

---
+++ kadai_005/score.php
@@ -0,0 +1,29 @@
+<?php
+// 変数に値を代入する
+  $score1 = 80;
+  $score2 = 60;
+  $score3 = 55;
+  $score4 = 40;
+  $score5 = 100;
+  $score6 = 25;
+  $score7 = 80;
+  $score8 = 95;
+  $score9 = 30;
+  $score10 = 60;
+
+  echo '<br>';
+
+  // 四則演算
+  
+  $total = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+  
+  // 合計
+  echo $total;
+
+  echo '<br>';
+
+  // 平均
+  echo $total / 10;
+
+
+?>
